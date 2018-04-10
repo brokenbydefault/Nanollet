cbdea7617731ebe5243f5be8aee4b74dba2b8749
Inkeliz <lucasrod2010@aol.com>
2018-04-10 18:18:37 -0300

    tweak: ignore pinning when debug is enabled

--- RPC/internal/keypinning.go
+++ RPC/internal/keypinning.go
@@ -6,12 +6,18 @@ import (
 	"github.com/brokenbydefault/Nanollet/Util"
 	"errors"
 	"crypto/subtle"
+	"github.com/brokenbydefault/Nanollet/Config"
 )
 
 var ErrInvalidCert = errors.New("invalid certificate")
 
 func VerifyPeerCertificate(expectedhash []byte, uri string) func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
 	return func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
+		// KeyPinning is ignored when debug is enabled
+		if Config.IsDebugEnabled() {
+			return nil
+		}
+
 		link, err := url.Parse(uri)
 		if err != nil {
 			panic("the connection url is invalid")
