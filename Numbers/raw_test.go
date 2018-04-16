2d7d43c923c556ca8760cbae9f19f2b2c0b1356e
Inkeliz <lucasrod2010@aol.com>
2018-04-15 21:04:43 -0300

    add: isValid() testing

--- Numbers/raw_test.go
+++ Numbers/raw_test.go
@@ -1,13 +1,34 @@
 package Numbers
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
 func TestNewRawFromString(t *testing.T) {
 
-	n, _ := NewRawFromString("1")
-	fmt.Println(n.ToHex())
+	n, err := NewRawFromString("340282366920937463463374607431768211456")
+	if err != nil {
+		panic(err)
+	}
+	x, err  := NewRawFromString("340282366920938463463374607431768211455")
+	if err != nil {
+		panic(err)
+	}
+	n = n.Add(x)
+	fmt.Println(n.ToString(), n.IsValid())
+	fmt.Println(n.ToBytes())
+
+}
+
+func TestRawAmount_IsValid(t *testing.T) {
+
+	if _, err := NewRawFromString("-1"); err == nil  {
+		t.Error("is valid a invalid amount")
+	}
+
+	if _, err := NewRawFromHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"); err == nil  {
+		t.Error("is valid a invalid amount")
+	}
 
 }
