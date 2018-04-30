a18fef8cf4b66713dc97189be9b13ae54cce7308
Inkeliz <lucasrod2010@aol.com>
2018-04-29 21:31:42 -0300

    add: default representative

--- Config/config.go
+++ Config/config.go
@@ -1,6 +1,9 @@
 package Config
 
+import "github.com/brokenbydefault/Nanollet/Wallet"
+
 var Debug = false
+var DefaultRepresentative = Wallet.Address("xrb_1ywcdyz7djjdaqbextj4wh1db3wykze5ueh9wnmbgrcykg3t5k1se7zyjf95")
 
 func IsDebugEnabled() bool {
 	return Debug
