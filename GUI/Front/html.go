40a9aa5e07ae578e9b35d4996cd0e1cf8b0dbf0b
Inkeliz <inkeliz@inkeliz.com>
2018-09-15 12:19:26 -0300

    Update pre-generated files
    This files are generated by go generate.

--- GUI/Front/html.go
+++ GUI/Front/html.go
@@ -10,24 +10,23 @@ var HTMLAccount = HTMLPAGE(`<main application="account">
         <div class="middle">
 
             <label>LICENSE</label>
-            <tos class="tos" spellcheck="false" readonly>MIT License
+            <tos class="tos">MIT License
+Copyright (c) 2018 Inkeliz
 
-                Copyright (c) 2018 Inkeliz
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+following conditions:
 
-                Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-                associated documentation files (the "Software"), to deal in the Software without restriction, including
-                without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-                copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
-                following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
 
-                The above copyright notice and this permission notice shall be included in all copies or substantial
-                portions of the Software.
-
-                THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
-                LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-                NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-                WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-                SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
             </tos>
             <input class="accept" type="submit" value="ACCEPT AND CONTINUE">
 
@@ -148,8 +147,14 @@ var HTMLBase = HTMLPAGE(`<html window-frame="solid" window-blurbehind="light" re
         NANOLLET
     </section>
     <section class="account">
-        <icon class="logo-nano"/>
-        <div class="ammount"></div>
+        <div class="box">
+            <icon class="icon-coins"/>
+            <div class="ammount"> </div>
+        </div>
+        <div class="box">
+            <icon class="icon-nodes"/>
+            <div class="nodes"> </div>
+        </div>
         <button role="window-close">
             <icon id="end" class="icon-exit"></icon>
         </button>
