86e7454478a69ae85e83badc2eca787396f431d5
Inkeliz <inkeliz@inkeliz.com>
2018-10-21 04:06:27 -0300

    add: error when no elements
    Update from go-sciter (https://github.com/sciter-sdk/go-sciter/commit/d48763867dab369bb7d7395cdebae3b690b9ce6d) removes the error when no element is found.

--- GUI/App/DOM/select.go
+++ GUI/App/DOM/select.go
@@ -2,13 +2,25 @@
 
 package DOM
 
+import "errors"
+
+var (
+	ErrInvalidElement = errors.New("invalid element")
+)
+
 func (el *Element) SelectAllElement(css string) ([]*Element, error) {
 	e, err := el.el.Select(css)
+	if e == nil {
+		err = ErrInvalidElement
+	}
 	return NewElements(e), err
 }
 
 func (el *Element) SelectFirstElement(css string) (*Element, error) {
 	e, err := el.el.SelectFirst(css)
+	if e == nil {
+		err = ErrInvalidElement
+	}
 	return NewElement(e), err
 }
 
